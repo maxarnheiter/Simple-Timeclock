fb49a0efb884c38be5e52146a69c9c4c02be989d
maxarnheiter <maxarnheiter@gmail.com>
2015-04-21 12:51:58 -0400

    1. Initial UI building

---
+++ SimpleTimeClock/SimpleTimeClock/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTimeClock
+{
+    class Employee
+    {
+        public string fname = "";
+        public string lname = "";
+
+        public string fullname
+        {
+            get { return fname + " " + lname; }
+        }
+
+        public Employee(string f_name, string l_name)
+        {
+            this.fname = f_name;
+            this.lname = l_name;
+        }
+
+
+    }
+}
